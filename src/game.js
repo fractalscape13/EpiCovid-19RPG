0367ee8ed8bc07a771bf12d5598dca9e11297dd0
Patrick Kille <pr.kille@gmail.com>
2020-04-15 14:19:27 -0700

    ad functions for hygiene and doctor visit
    Co-Authored-by: <zakkreyjshort@gmail.com>
    Co-Authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-Authored-by: Michelle Morin <morin2m@gmail.com>

--- src/game.js
+++ src/game.js
@@ -6,6 +6,7 @@ export const changeState = (prop) => {
     })
   }
 }
+
 export const changeStringState = (prop) => {
   return (value) => {
     return (state) => ({
@@ -25,10 +26,21 @@ export const storeState = (initialValues) => {
 }
 
 // Initial player values:
-const initialValues = { hygiene: 10, foodSupply: 5, tpSupply: 5 };
+const initialValues = { hygiene: 10, foodSupply: 5, tpSupply: 5, doctorAllowance: true };
+export const myPlayer = storeState(initialValues);
 
 // Assign player a name:
 export const assignName = changeStringState("name");
 
 // Functions that affect hygiene:
 export const noShower = changeState("hygiene")(-1);
+export const shower = changeState("hygiene")(3);
+export const groceryRun = changeState("hygiene")(-2);
+export const visitDoctorAndGetBetter = changeState("hygiene")(10);
+
+// functions that affect foodSupply:
+export const buyFood = changeState("foodSupply")(4);
+export const consumeFood = changeState("foodSupply")(-1);
+
+// functions that affect doctor:
+export const visitDoctorAndUseAllowance = changeStringState("doctorAllowance")(false);
