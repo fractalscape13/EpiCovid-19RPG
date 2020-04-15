ec89d38f44d4e234baadf621afd47cc5cc45fa47
Patrick Kille <pr.kille@gmail.com>
2020-04-15 14:47:23 -0700

    add functions and first test for tp supply
    Co-Authored-by: Michelle Morin <morin2m@gmail.com>
    Co-Authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-Authored-by: <zakkreyjshort@gmail.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,23 +1,37 @@
-import { visitDoctorAndGetBetter, changeState, noShower, assignName, storeState, myPlayer, visitDoctorAndUseAllowance } from './../src/game.js';
+import { visitDoctorAndGetBetter, goShower, changeState, noShower, groceryRun, assignName, storeState, myPlayer, visitDoctorAndUseAllowance, initialValues, buyFood, consumeFood, buyTpSupply, sellTpForDoctor, dailyUseOfTp, tradeForBidet} from './../src/game.js';
 
-describe('noShower', () => {
+describe('hygiene modifications', () => {
 
   test('should decrement hygiene of player by 1', () => {
-    const updatedPlayerObj = myPlayer(noShower);
+    const firstPlayer = storeState(initialValues);
+    const updatedPlayerObj = firstPlayer(noShower);
     expect(updatedPlayerObj.hygiene).toEqual(9);
+  });
+
+  test('should increment hygiene of player by 3', () => {
+    const secondPlayer = storeState(initialValues);
+    const cleanPlayerObj = secondPlayer(goShower);
+    expect(cleanPlayerObj.hygiene).toEqual(13);
+  });
+
+  test('should decrement hygiene by 2', () => {
+    const fourthPlayer = storeState(initialValues);
+    const playerWithGroceries = fourthPlayer(groceryRun);
+    expect(playerWithGroceries.hygiene).toEqual(8);
   });
 });
 
 describe('assignName', () => {
 
   test('should set a player name when no name is assigned', () => {
+    const thirdPlayer = storeState(initialValues);
     const newName = assignName("Patrick");
-    const updatedPlayerObj = myPlayer(newName);
+    const updatedPlayerObj = thirdPlayer(newName);
     expect(updatedPlayerObj.name).toEqual("Patrick");
   });
 });
 
-describe('allow user to go to doctor and restore hygiene', () => {
+describe('doctor visits', () => {
 
   test('should test whether player can get sick', () => {
     const sickInitialValues = {hygiene: 1, foodSupply: 5, tpSupply: 5, doctorAllowance: true};
@@ -25,14 +39,35 @@ describe('allow user to go to doctor and restore hygiene', () => {
     const recoveredPlayer = sickPlayer(visitDoctorAndGetBetter);
     expect(recoveredPlayer.hygiene).toEqual(11);
   });
-});
-
-describe('remove doctor availability when user goes to doctor', () => {
   
   test('should change doctor availability from true to false', () => {
     const sickValues = {hygiene: 1, foodSupply: 5, tpSupply: 5, doctorAllowance: true};
     const newSickPlayer = storeState(sickValues);
     const recoveredPlayerVisitedDoctor = newSickPlayer(visitDoctorAndUseAllowance);
-    expect(recoveredPlayerVisitedDoctor.doctorAllowance).toEqual(true);
+    expect(recoveredPlayerVisitedDoctor.doctorAllowance).toEqual(false);
   });
 });
+
+describe('modifications to food supply', () => {
+
+  test('should increment food supply by 4', () => {
+    const fifthPlayer = storeState(initialValues);
+    const playerBoughtFood = fifthPlayer(buyFood);
+    expect(playerBoughtFood.foodSupply).toEqual(9);
+  });
+
+  test('should decrement food supply by 1', () => {
+    const sixthPlayer = storeState(initialValues);
+    const playerConsumedFood = sixthPlayer(consumeFood);
+    expect(playerConsumedFood.foodSupply).toEqual(4);
+  });
+});
+
+describe('modifications to tp supply', () => {
+
+  test('should increment tp by 1', () => {
+    const seventhPlayer = storeState(initialValues);
+    const playerBoughtTp = seventhPlayer(buyTpSupply);
+    expect(playerBoughtTp.tpSupply).toEqual(6);
+  })
+})
--- src/game.js
+++ src/game.js
@@ -26,15 +26,14 @@ export const storeState = (initialValues) => {
 }
 
 // Initial player values:
-const initialValues = { hygiene: 10, foodSupply: 5, tpSupply: 5, doctorAllowance: true };
-export const myPlayer = storeState(initialValues);
+export const initialValues = { hygiene: 10, foodSupply: 5, tpSupply: 5, doctorAllowance: true };
 
 // Assign player a name:
 export const assignName = changeStringState("name");
 
 // Functions that affect hygiene:
 export const noShower = changeState("hygiene")(-1);
-export const shower = changeState("hygiene")(3);
+export const goShower = changeState("hygiene")(3);
 export const groceryRun = changeState("hygiene")(-2);
 export const visitDoctorAndGetBetter = changeState("hygiene")(10);
 
@@ -44,3 +43,9 @@ export const consumeFood = changeState("foodSupply")(-1);
 
 // functions that affect doctor:
 export const visitDoctorAndUseAllowance = changeStringState("doctorAllowance")(false);
+
+// functions that affect tp supply:
+export const buyTpSupply = changeState("tpSupply")(1);
+export const sellTpForDoctor = changeState("tpSupply")(-5);
+export const dailyUseOfTp = changeState("tpSupply")(-1);
+export const tradeForBidet = changeState("tpSupply")(-15);
