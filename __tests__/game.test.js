506850c92dc888dd420c08f970503aa5a840b16c
Patrick Kille <pr.kille@gmail.com>
2020-04-15 14:18:24 -0700

    add tests for doctor visits
    Co-Authored-by: Michelle Morin <morin2m@gmail.com>
    Co-Authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-Authored-by: <zakkreyjshort@gmail.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,10 +1,8 @@
-import { storeState, noShower, assignName } from './../src/game.js';
+import { visitDoctorAndGetBetter, changeState, noShower, assignName, storeState, myPlayer, visitDoctorAndUseAllowance } from './../src/game.js';
 
 describe('noShower', () => {
 
   test('should decrement hygiene of player by 1', () => {
-    const initialValues = {hygiene: 10};
-    const myPlayer = storeState(initialValues);
     const updatedPlayerObj = myPlayer(noShower);
     expect(updatedPlayerObj.hygiene).toEqual(9);
   });
@@ -12,18 +10,29 @@ describe('noShower', () => {
 
 describe('assignName', () => {
 
-  test('should change name of player', () => {
-    const initialValues = {name: "Randy"};
-    const myPlayer = storeState(initialValues);
+  test('should set a player name when no name is assigned', () => {
     const newName = assignName("Patrick");
     const updatedPlayerObj = myPlayer(newName);
     expect(updatedPlayerObj.name).toEqual("Patrick");
   });
+});
 
-  test('should set a player name when no name is assigned', () => {
-    const myPlayer = storeState({});
-    const newName = assignName("Patrick");
-    const updatedPlayerObj = myPlayer(newName);
-    expect(updatedPlayerObj.name).toEqual("Patrick");
+describe('allow user to go to doctor and restore hygiene', () => {
+
+  test('should test whether player can get sick', () => {
+    const sickInitialValues = {hygiene: 1, foodSupply: 5, tpSupply: 5, doctorAllowance: true};
+    const sickPlayer = storeState(sickInitialValues);
+    const recoveredPlayer = sickPlayer(visitDoctorAndGetBetter);
+    expect(recoveredPlayer.hygiene).toEqual(11);
+  });
+});
+
+describe('remove doctor availability when user goes to doctor', () => {
+  
+  test('should change doctor availability from true to false', () => {
+    const sickValues = {hygiene: 1, foodSupply: 5, tpSupply: 5, doctorAllowance: true};
+    const newSickPlayer = storeState(sickValues);
+    const recoveredPlayerVisitedDoctor = newSickPlayer(visitDoctorAndUseAllowance);
+    expect(recoveredPlayerVisitedDoctor.doctorAllowance).toEqual(true);
   });
 });
