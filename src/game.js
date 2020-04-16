572d2e613b407530eb9261efe9679270e2d1dd83
Patrick Kille <pr.kille@gmail.com>
2020-04-16 11:12:00 -0700

    refactor functions to change multiple properties
    Co-Authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-Authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-Authored-by: Michelle Morin <morin2m@gmail.com>

--- src/game.js
+++ src/game.js
@@ -45,6 +45,39 @@ export const changeNumericalandStringProperty = (numProp) => {
   }
 }
 
+const changeTwoNumberProps = (numProp1) => {
+  return (numVal1) => {
+    return (numProp2) => {
+      return (numVal2) => {
+        return (state) => ({
+          ...state,
+          [numProp1] : (state[numProp1] || 0) + numVal1,
+          [numProp2] : (state[numProp2] || 0) + numVal2
+        });
+      }
+    }
+  }
+}
+
+const changeTwoNumberAndOneStringProperties = (numProp1) => {
+  return (numVal1) => {
+    return (numProp2) => {
+      return (numVal2) => {
+        return (stringProp) => {
+          return (stringVal) => {
+            return (state) => ({
+              ...state,
+              [numProp1] : (state[numProp1] || 0) + numVal1,
+              [numProp2] : (state[numProp2] || 0) + numVal2,
+              [stringProp] : stringVal
+            });
+          }
+        }
+      }
+    }
+  }
+}
+
 export const storeState = (initialValues) => {
   let currentState = initialValues;
   return (stateChangeFunction) => {
@@ -56,27 +89,24 @@ export const storeState = (initialValues) => {
 
 // Initial player values:
 export const initialValues = { hygiene: 10, foodSupply: 5, tpSupply: 5, doctorAllowance: true, bidet: false, pet: false, garden: false, trowel: false, leash: false, dayCount: 0 };
+
 // Assign player a name:
 export const assignName = changeStringState("name");
 
 // Functions that affect hygiene:
 export const noShower = changeState("hygiene")(-1);
 export const goShower = changeState("hygiene")(3);
-export const groceryRun = changeState("hygiene")(-2);
-export const visitDoctorAndGetBetter = changeState("hygiene")(10);
+export const groceryRun = changeTwoNumberProps("hygiene")(-2)("foodSupply")(4);
 
 // functions that affect foodSupply:
-export const buyFood = changeState("foodSupply")(4);
 export const consumeFood = changeState("foodSupply")(-1);
 
 // functions that affect doctor:
-export const visitDoctorAndUseAllowance = changeStringState("doctorAllowance")(false);
+export const visitDoctor = changeTwoNumberAndOneStringProperties("hygiene")(10)("tpSupply")(-5)("doctorAllowance")(false);
 
 // functions that affect tp supply:
 export const buyTpSupply = changeState("tpSupply")(1);
-export const sellTpForDoctor = changeState("tpSupply")(-5);
 export const dailyUseOfTp = changeState("tpSupply")(-1);
-export const tradeForBidet = changeState("tpSupply")(-15);
 
 // increment day count:
 export const nextDay = changeState("dayCount")(1);
